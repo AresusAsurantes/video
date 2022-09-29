a5e37c714b2000d487d3eecdab33d18eb91b9850
AresusAsurantes <aresus331@gmail.com>
2022-09-29 18:15:01 +0800

    new program

---
+++ video/config-service/src/main/java/com/video/config/ConfigServer.java
@@ -0,0 +1,9 @@
+package com.video.config;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServer {
+}
