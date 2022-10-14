991d3d7b79eff9cd0f6e85a73b192eb511d80f91
刘昌 <1295447578@qq.com>
2022-10-14 17:12:43 +0800

    "搭建基础工程"

--- video/eureka-server/src/main/java/com/video/regist/EurekaServer.java
+++ video/eureka-server/src/main/java/com/video/regist/EurekaServer.java
@@ -1,13 +1,16 @@
 package com.video.regist;
 
+import com.video.regist.test.TestApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServer {
+
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServer.class, args);
+        SpringApplication.run(EurekaServer.class, args);;
     }
 }
